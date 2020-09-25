558c764716aaca4b24525b799fc344d18a8f6fe2
Davi Costa <davi.machado.costa@hotmail.com>
2020-09-25 11:41:37 -0300

    Criando novos triggers

--- Oracle/Livraria/Triggers.sql
+++ Oracle/Livraria/Triggers.sql
@@ -20,3 +20,101 @@ BEGIN
 END;
 
 INSERT INTO TB_AUTOR VALUES (SQ_AUTOR.NEXTVAL, 'Mario', 'M', '10.10.2012');
+
+-- Cria uma tupla na tabela log para registrar quem apagou a tupla da editora
+CREATE OR REPLACE TRIGGER TB_LOG_EDITORA
+BEFORE DELETE
+ON TB_EDITORA
+FOR EACH ROW
+BEGIN
+    INSERT INTO TB_LOG VALUES (SQ_LOG.NEXTVAL, USER, SYSDATE, :OLD.DESCRICAO) ;
+END;
+
+SELECT * FROM TB_LOG;
+SELECT * FROM TB_EDITORA;
+
+DELETE FROM TB_EDITORA WHERE ID_EDITORA = 3;
+
+-- Nao permite que o preco de um livro seja reajustado em mais de 50%
+CREATE OR REPLACE TRIGGER TB_LIMITE_REAJUSTE_LIVRO
+BEFORE UPDATE
+ON TB_LIVRO
+FOR EACH ROW
+BEGIN
+    IF(:NEW.preco > :OLD.preco * 1.5) THEN
+        RAISE_APPLICATION_ERROR(-20334, 'Reajuste nao permitido!!');
+    END IF;
+END;
+
+-- Da baixa no estoque do livro ao criar um pedido
+CREATE OR REPLACE TRIGGER TB_BAIXA_ESTOQUE
+BEFORE INSERT
+ON TB_ITENS_PEDIDO
+FOR EACH ROW
+DECLARE 
+    estoque TB_LIVRO.QTDE_ESTOQUE%TYPE;
+BEGIN
+    SELECT 
+        QTDE_ESTOQUE
+    INTO
+        estoque
+    FROM 
+        TB_LIVRO 
+    WHERE 
+        ID_LIVRO = :NEW.ID_LIVRO;
+        
+    IF(estoque - :NEW.QUANTIDADE < 0) THEN
+        RAISE_APPLICATION_ERROR(-20334, 'A quantidade de livros do pedido ultrapassa a quantidade em estoque!!');
+    ELSE
+        UPDATE 
+            TB_LIVRO
+        SET
+            QTDE_ESTOQUE = QTDE_ESTOQUE - :NEW.QUANTIDADE
+        WHERE
+            ID_LIVRO = :NEW.ID_LIVRO;
+    END IF;
+END;
+
+INSERT INTO TB_ITENS_PEDIDO VALUES (SQ_ITENS_PEDIDO.NEXTVAL,1,1,1,100.00);
+
+SELECT * FROM TB_LIVRO;
+
+-- Nao permite pedidos que custam mais de 500 reais
+CREATE OR REPLACE TRIGGER TB_LIMITE_PEDIDO
+BEFORE INSERT OR UPDATE
+ON TB_ITENS_PEDIDO
+FOR EACH ROW
+DECLARE valor NUMBER;
+BEGIN
+
+    SELECT 
+        SUM(PRECO)
+    INTO
+        valor
+    FROM 
+        TB_ITENS_PEDIDO
+    WHERE 
+        ID_PEDIDO = :NEW.ID_PEDIDO;
+        
+    valor := valor + :NEW.PRECO;
+    
+    IF(valor > 500) THEN
+        RAISE_APPLICATION_ERROR(-20334, 'O preco do pedido excede 500 reais!!');
+    END IF;
+    
+END;
+
+INSERT INTO TB_ITENS_PEDIDO VALUES (SQ_ITENS_PEDIDO.NEXTVAL,1,1,1,50.00);
+
+-- Muda a data de entrega automaticamente para 5 dias após a data do pedido
+CREATE OR REPLACE TRIGGER TR_DATA_ENTREGA
+BEFORE INSERT
+ON TB_PEDIDO
+FOR EACH ROW
+BEGIN
+    :NEW.DATA_ENTREGA := :NEW.DATA_PEDIDO + 5;
+END;
+
+INSERT INTO TB_PEDIDO VALUES (SQ_PEDIDO.NEXTVAL, SYSDATE, SYSDATE);
+
+SELECT * FROM TB_PEDIDO;
