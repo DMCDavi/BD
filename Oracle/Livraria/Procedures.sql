ba03e248b592d56002c6853df022bbd362affb7d
Davi Costa <davi.machado.costa@hotmail.com>
2020-10-02 12:09:36 -0300

    Criando uma procedure que cria uma tabela
    Retirei também as funções do arquivo das procedures já que elas estão num arquivo separado

--- Oracle/Livraria/Procedures.sql
+++ Oracle/Livraria/Procedures.sql
@@ -51,24 +51,6 @@ END;
 -- Executa uma procedure fora de um bloco anonimo
 EXEC SP_CALCULA_AREA;
 
--- Criando uma funcao
-CREATE OR REPLACE FUNCTION FC_CALCULA_AREA_RETANGULO
-(
-    vBase number,
-    vAltura number default 4
-)
-RETURN NUMBER
-AS vArea number;
-BEGIN
-    vArea := vBase * vAltura;
-    RETURN vArea;
-END;
-
--- Diferente de procedures, as funcoes podem ser chamadas com comandos SQL
-SELECT FC_CALCULA_AREA_RETANGULO(2,5) FROM DUAL;
-
-SELECT FC_CALCULA_AREA_RETANGULO(2) FROM DUAL;
-
 -- Chama uma funcao dentro de uma procedure
 CREATE OR REPLACE PROCEDURE SP_CALCULA_AREA2
 AS vArea number;
@@ -79,34 +61,45 @@ END;
 
 EXEC SP_CALCULA_AREA2;
 
--- Eleva o preco ao quadrado
-CREATE OR REPLACE FUNCTION FC_PRECO_AO_QUADRADO
-(
-    pPreco number
-)
-RETURN number
+-- Da permissao para que o usuario crie uma tabela dentro de uma procedure
+GRANT CREATE TABLE TO C##DAVI;
+
+-- Cria uma nova tabela de acordo aos nomes das editoras
+CREATE OR REPLACE PROCEDURE PR_DYNAMIC_SQL
 AS
+    vDescricao TB_EDITORA.DESCRICAO%TYPE;
+    vComand VARCHAR2(32767);
+    CURSOR cEditoras IS
+    SELECT
+        DESCRICAO
+    FROM
+        TB_EDITORA;
 BEGIN
-    RETURN pPreco**2;
+    vComand := 'CREATE TABLE NOVA_TABELA (';
+    
+    -- Preenche os atributos da tabela de acordo aos nomes das editoras
+    OPEN cEditoras;
+    LOOP
+        FETCH cEditoras INTO vDescricao;
+        EXIT WHEN cEditoras%NOTFOUND;
+        
+        -- Troca os espacos em branco com underline
+        vDescricao := REPLACE (vDescricao, ' ', '_');
+        -- Coloca o nome da editora como atributo
+        vComand := CONCAT(vComand, vDescricao);
+        -- Coloca o tipo do atributo
+        vComand := CONCAT(vComand, ' INT,');
+        
+    END LOOP;
+    CLOSE cEditoras;
+    
+    -- Retira a ultima virgula
+    vComand := SUBSTR(vComand, 0, LENGTH(vComand) - 1);
+    -- Finaliza o comando
+    vComand := CONCAT(vComand, ')');
+    
+    -- Cria a tabela
+    EXECUTE IMMEDIATE vComand;
 END;
 
--- Chamando funcao com o campo de uma tabela
-SELECT FC_PRECO_AO_QUADRADO(L.PRECO) FROM TB_LIVRO L;
-
--- Conta a quantidade de tuplas que uma tabela possui
-CREATE OR REPLACE FUNCTION FC_CONTADOR_REGISTROS
-(
-    pTabela VARCHAR2
-)
-RETURN INTEGER
-IS vRetorno INTEGER;
-BEGIN
-    -- Transforma uma string num comando
-    EXECUTE IMMEDIATE
-        'SELECT COUNT(*)
-            FROM ' || pTabela
-        INTO vRetorno;
-    RETURN vRetorno;
-END;
-
-SELECT FC_CONTADOR_REGISTROS('TB_LIVRO_AUTOR') FROM DUAL;
+EXEC PR_DYNAMIC_SQL;
